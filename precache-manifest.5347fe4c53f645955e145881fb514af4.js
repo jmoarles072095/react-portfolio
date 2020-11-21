ded476f78977b7710285e1da94953076c5976984
Jose Morales <jmorales072095@gmail.com>
2020-11-20 23:02:54 -0800

    Updates

---
+++ precache-manifest.5347fe4c53f645955e145881fb514af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4a549a7d1296173cdbd23fc912fa23",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "33b7f9246e166bd8425f",
+    "url": "/react-portfolio/static/css/main.8b5d00fa.chunk.css"
+  },
+  {
+    "revision": "a153eb4c037b50678a84",
+    "url": "/react-portfolio/static/js/2.244d1b82.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-portfolio/static/js/2.244d1b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b7f9246e166bd8425f",
+    "url": "/react-portfolio/static/js/main.7027a4e3.chunk.js"
+  },
+  {
+    "revision": "26950ce1f34a1b315732",
+    "url": "/react-portfolio/static/js/runtime-main.e44c21d0.js"
+  }
+]);
