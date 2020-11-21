e085085b635ebb4ea9d9b95f6ca2acf9dfe6cfd8
Jose Morales <jmorales072095@gmail.com>
2020-11-20 18:28:51 -0800

    added and updated code

---
+++ src/components/yourdata.js
@@ -0,0 +1,49 @@
+export default {
+    name: "JLMM",
+
+    //Contact Email
+    contactEmail: "jmorales072095@gmail.com",
+    landingPageName: "Jose L. Morales",
+    landingPagePara: "Full Stack developer. My values center in helping users achieving what I can help them with and if they can't figure it out, I will for them ",
+
+    // Landing Page Image (replace - root/public/assets/photo.jpg)
+    landingPageImage: "assets/JoseM.jpeg",
+
+    projects: [{
+            id: 1,
+            title: "Project One", //Project Title - Add Your Project Title Here
+            imageSrc: "./assets/gymbro.png",
+            //Project URL - Add Your Project Url Here
+            url: "https://dan-gentile.github.io/gym-bro/index.html"
+        },
+        {
+            id: 2, //DO NOT CHANGE THIS (Please)😅
+            title: "Project Two",
+            imageSrc: "./assets/geromescreenshot.png",
+            url: "https://gerome-the-gnome.herokuapp.com/"
+        },
+        {
+            id: 3, //DO NOT CHANGE THIS (Please)😅
+            title: "Project Three",
+            imageSrc: "./assets/5Day.png",
+            url: "https://jmoarles072095.github.io/weather-forecast/."
+        }
+    ],
+
+    // Contact Section
+    social: [
+        // Add Or Remove The Link Accordingly
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com/in/chetanverma7/"
+        },
+        {
+            name: "Github",
+            url: "https://www.behance.net/chetanverma"
+        },
+    ],
+
+    // About Page
+    aboutTitle: "",
+    aboutPara: "HELLO"
+};
